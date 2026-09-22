492a04210b46accd5cd422f7f54d50d5692ffdfe
Lokesh <lokeshwebdeveloper26@gmail.com>
2026-09-22 16:21:40 +0530

    Update portfolio

--- data/model-data.ts
+++ data/model-data.ts
@@ -64,11 +64,11 @@ export const modelData: ModelData = {
   instagram: "v.inayak_s",
 
   ecommerce: [
-  /**{
+  {
     name: "Flipkart",
-    url: "https://www.flipkart.com/",
-    description: "Shop featured products and collections.",
-  },*/
+    url: "https://www.flipkart.com/product/p/itme?pid=KPBHR6TJYKXZCH44",
+    description: "Shop featured products and collections. Cubby Boys Casual T-shirt Shorts (Multicolor)",
+  },
 ],
 
   /**
